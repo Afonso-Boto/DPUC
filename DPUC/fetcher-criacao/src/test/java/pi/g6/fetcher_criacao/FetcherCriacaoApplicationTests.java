f7f963aa9d3437b6c3bbdc9483770ace64e00c7f
Tomás Candeias <tomaslcandeias@outlook.com>
2022-04-08 18:31:34 +0100

    project structure: fetchers and teacher app

---
+++ DPUC/fetcher-criacao/src/test/java/pi/g6/fetcher_criacao/FetcherCriacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package pi.g6.fetcher_criacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetcherCriacaoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
