836ce39a9aeefcb99a59cbd0d80c05e9198b67a5
Jorge Oliveira <jorge.am.oliveira@ua.pt>
2022-05-02 18:55:09 +0100

    Base for authentication and navbar

--- DPUC/teacher-app/src/App.js
+++ DPUC/teacher-app/src/App.js
@@ -6,13 +6,34 @@ import CreateUC from './CreateUC';
 import NotFound from './NotFound';
 import Home from './Home';
 import ViewDPUC from './ViewDPUC';
-
+import "@uaveiro/systems-bar";
 
 function App() {
   return (
       <Router>
         <div className="App">
+          {/* 
+          <ua-systems-bar
+            publicLinks={JSON.stringify([
+              { text: "My Custom Link", href: "#" },
+              { text: "Another Custom Link", href: "#" },
+            ])}
+            userLinks={JSON.stringify([
+              { text: "Open link", href: "#" },
+              { text: "Custom Link", href: "#" },
+            ])}
+            lang="pt"
+            containerFluid
+            authConfig={JSON.stringify({
+              url: "https://wso2-is.dev.ua.pt",
+              clientId: "D2wPaAQ3_dfgJgeStXAfwJRCKu0a",
+              idToken: localStorage.getItem("sb-ua-auth-id-token"),
+              callbackUri: "http://localhost:8081/pt/login",
+            })}
+          />
+          */}
           <div className="content">
+            
             <Container>
             <Switch>
               <Route exact path="/" element={<Home />}/>
