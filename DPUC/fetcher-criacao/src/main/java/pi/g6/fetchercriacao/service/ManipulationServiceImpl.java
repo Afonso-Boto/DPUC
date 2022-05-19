0c60dfeedaf910fab55bfda87fd2d419ae958d9b
Tomás Candeias <tomaslcandeias@outlook.com>
2022-05-19 23:14:21 +0100

    db update

--- DPUC/fetcher-criacao/src/main/java/pi/g6/fetchercriacao/service/ManipulationServiceImpl.java
+++ DPUC/fetcher-criacao/src/main/java/pi/g6/fetchercriacao/service/ManipulationServiceImpl.java
@@ -3,9 +3,13 @@ package pi.g6.fetchercriacao.service;
 import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import pi.g6.fetchercriacao.entity.Estado;
 
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class ManipulationServiceImpl extends JdbcDaoSupport implements ManipulationService{
     @Autowired
@@ -19,7 +23,18 @@ public class ManipulationServiceImpl extends JdbcDaoSupport implements Manipulat
 
     @Override
     public void aprovarDpuc(int id) {
+        String sql = "SELECT * FROM estado";
+        List<Map<String, Object>> rows = getJdbcTemplate().queryForList(sql);
 
+        List<Estado> result = new ArrayList<Estado>();
+        for (Map<String, Object> row : rows) {
+            Estado estado = new Estado();
+            estado.setId((int) row.get("id"));
+            estado.setNome((String) row.get("nome"));
+            estado.setDescricao((String) row.get("descricao"));
+
+            result.add(estado);
+        }
     }
 
     @Override
