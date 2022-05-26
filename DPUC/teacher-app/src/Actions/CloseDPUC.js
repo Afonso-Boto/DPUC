c22214ca8122e85439f01b34b45f0e6554a8f730
Jorge Oliveira <jorge.am.oliveira@ua.pt>
2022-05-26 12:26:24 +0100

    Added Approval and Disable DPUC functionalities

--- DPUC/teacher-app/src/Actions/CloseDPUC.js
+++ DPUC/teacher-app/src/Actions/CloseDPUC.js
@@ -1,11 +1,36 @@
 import { Button, Text, FormInput } from "@paco_ua/pacoui";
 import { Modal, Col, Row } from "react-bootstrap";
 import { useState } from "react";
+import axios from "axios";
 
-const CloseDPUC = (id) => {
+
+const CloseDPUC = ({id}) => {
+    const BASE_URL = "http://localhost:82/creation/desativarDpuc?id=" + id;
+
     const [show, setShow] = useState(false);
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(false);
+
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+
+    const close = () => {
+        console.log(BASE_URL)
+        axios
+            .put(BASE_URL)
+            .then(() => {
+                handleClose();
+            })
+            .catch((error) => {
+                setError(true);
+                console.log(error);
+            })
+            .finally( () => {
+                setLoading(false);
+            });
+    }
+
     return ( 
         <>
             <Button danger style={{fontSize:"100%"}} onClick={handleShow} >
@@ -34,7 +59,7 @@ const CloseDPUC = (id) => {
                     <Button action style={{fontSize:"100%"}} onClick={handleClose} >
                         Cancelar
                     </Button>
-                    <Button danger style={{fontSize:"100%"}} onClick={handleClose} >
+                    <Button danger style={{fontSize:"100%"}} onClick={close} >
                         Fechar DPUC
                     </Button>
                 </Modal.Footer>
