b54159efd2fa9edcbbea4dbaa361cc294efd66bd
Tomás Candeias <tomaslcandeias@outlook.com>
2022-05-28 16:14:40 +0100

    edition service finished & db estados updated

--- DPUC/fetcher/src/main/java/pi/g6/fetcher/controller/EditionController.java
+++ DPUC/fetcher/src/main/java/pi/g6/fetcher/controller/EditionController.java
@@ -3,6 +3,8 @@ package pi.g6.fetcher.controller;
 import lombok.extern.log4j.Log4j2;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import pi.g6.fetcher.service.EditionService;
 
@@ -20,17 +22,22 @@ public class EditionController {
     }
 
     @PutMapping("/setRegente")
-    public void setRegente(@RequestParam("id") int id, @RequestParam("regenteid") int regenteid) {
-        editionService.setRegente(id, regenteid);
+    public HttpStatus setRegente(@RequestParam("id") int id, @RequestParam("regenteid") int regenteid) {
+        return editionService.setRegente(id, regenteid);
+    }
+
+    @PutMapping("/definicaoRegente")
+    public ResponseEntity<Integer> definicaoRegente(@RequestParam("id") int id, @RequestParam("regenteid") int regenteid) {
+        return editionService.definicaoRegente(id, regenteid);
     }
 
     @PutMapping("/emEdicao")
-    public void emEdicao(@RequestBody String json, @RequestParam("id") int id, @RequestParam("finished") boolean finished) {
-        editionService.emEdicao(id, new JSONObject(json), finished);
+    public HttpStatus emEdicao(@RequestBody String json, @RequestParam("id") int id, @RequestParam("finished") boolean finished) {
+        return editionService.emEdicao(id, new JSONObject(json), finished);
     }
 
     @PutMapping("/emAprovacao")
-    public void emAprovacao(@RequestParam("id") int id, @RequestParam("aprovado") boolean aprovado) {
-        editionService.emAprovacao(id, aprovado);
+    public HttpStatus emAprovacao(@RequestParam("id") int id, @RequestParam("aprovado") boolean aprovado) {
+        return editionService.emAprovacao(id, aprovado);
     }
 }
--- DPUC/fetcher/src/main/java/pi/g6/fetcher/service/EditionServiceImpl.java
+++ DPUC/fetcher/src/main/java/pi/g6/fetcher/service/EditionServiceImpl.java
@@ -1,29 +1,113 @@
 package pi.g6.fetcher.service;
 
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.time.LocalDate;
+
 @Service
-public class EditionServiceImpl implements EditionService {
+public class EditionServiceImpl extends JdbcDaoSupport implements EditionService {
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    CreationManipulationService creationManipulationService;
+
+    @PostConstruct
+    private void initialize(){
+        setDataSource(dataSource);
+    }
+
     @Override
     public HttpStatus setRegente(int dpucid, int regenteId) {
-        return null;
+        try{
+            String sql = "UPDATE dpuc SET utilizadoresid = ? WHERE id = ?";
+            getJdbcTemplate().update(sql, regenteId, dpucid);
+
+        }catch (Exception e){
+            logger.warn(e.getMessage());
+            return HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+
+        return HttpStatus.OK;
     }
 
     @Override
     public ResponseEntity<Integer> definicaoRegente(int dpucid, int regenteId) {
-        return null;
+        int new_dpucid = 0;
+        LocalDate date = LocalDate.now();
+
+        try{
+            // Criar novo dpuc com informação do antigo
+            final String sql = "INSERT INTO dpuc(criacao_edicao, duracao, carga_horaria, horas_contacto, horas_trabalho, objetivos, conteudos, coerencia_conteudos, metodologias, coerencia_metodologia, bibliografia, observacoes, regime_faltas, linguas, modalidade, requisitos, ficheiros, data_alteracao, pagina_publica, funcionamento, aprendizagem, estadoid, periodo_letivoid, UCid, utilizadoresid) select criacao_edicao, duracao, carga_horaria, horas_contacto, horas_trabalho, objetivos, conteudos, coerencia_conteudos, metodologias, coerencia_metodologia, bibliografia, observacoes, regime_faltas, linguas, modalidade, requisitos, ficheiros, data_alteracao, pagina_publica, funcionamento, aprendizagem, estadoid, periodo_letivoid, UCid, utilizadoresid FROM dpuc WHERE id = ?";
+            KeyHolder keyHolder = new GeneratedKeyHolder();
+            getJdbcTemplate().update(connection -> {
+                PreparedStatement ps = connection.prepareStatement(sql,
+                        Statement.RETURN_GENERATED_KEYS);
+
+                return ps;
+            }, keyHolder);
+            new_dpucid = keyHolder.getKey().intValue();
+
+            // Update estado para E2 - Em edição (8 na tabela de estados)
+            String sql2 = "UPDATE dpuc SET criacao_edicao=?, data_alteracao=?, utilizadoresid=?, estadoid=? WHERE id = ?";
+            getJdbcTemplate().update(sql2, 1, date.toString(), regenteId, 8, new_dpucid);
+
+        }catch (Exception e){
+            logger.warn(e.getMessage());
+            return new ResponseEntity<>(new_dpucid, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        return new ResponseEntity<>(new_dpucid, HttpStatus.OK);
     }
 
     @Override
     public HttpStatus emEdicao(int dpucid, JSONObject dpuc, boolean finished) {
-        return null;
+        try{
+            //Update dpuc
+            creationManipulationService.editarDpuc(dpuc, dpucid);
+
+            if (finished){
+                // Update estado para E3 - Em edição (9 na tabela de estados)
+                String sql = "UPDATE dpuc SET estadoid=? WHERE id = ?";
+                getJdbcTemplate().update(sql, 9, dpucid);
+            }
+
+        }catch (Exception e){
+            logger.warn(e.getMessage());
+            return HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+
+        return HttpStatus.OK;
     }
 
     @Override
     public HttpStatus emAprovacao(int dpucid, boolean aprovado) {
-        return null;
+        try{
+            String sql = "UPDATE dpuc SET estadoid=? WHERE id = ?";
+
+            if (aprovado){
+                // Update estado para E5 - Em aprovação (10 na tabela de estados)
+                getJdbcTemplate().update(sql, 10, dpucid);
+            }else{
+                // Update estado para E2 - Em edição (8 na tabela de estados)
+                getJdbcTemplate().update(sql, 8, dpucid);
+            }
+        }catch (Exception e){
+            logger.warn(e.getMessage());
+            return HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+
+        return HttpStatus.OK;
     }
 }
