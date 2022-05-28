106b0d3b7c9a8c61702beb2be2c7726a5ef78af2
Tomás Candeias <tomaslcandeias@outlook.com>
2022-05-28 14:03:43 +0100

    fetcher with swagger for API documentation

---
+++ DPUC/fetcher/src/main/java/pi/g6/fetcher/entity/Curso.java
@@ -0,0 +1,41 @@
+package pi.g6.fetcher.entity;
+
+public class Curso {
+    private int id;
+    private String nome;
+    private int unidade_organicaid;
+    private int utilizadores_id;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getUtilizadores_id() {
+        return utilizadores_id;
+    }
+
+    public void setUtilizadores_id(int utilizadores_id) {
+        this.utilizadores_id = utilizadores_id;
+    }
+
+    public int getUnidade_organicaid() {
+        return unidade_organicaid;
+    }
+
+    public void setUnidade_organicaid(int unidade_organicaid) {
+        this.unidade_organicaid = unidade_organicaid;
+    }
+}
---
+++ DPUC/fetcher/src/main/java/pi/g6/fetcher/service/CreationService.java
@@ -0,0 +1,18 @@
+package pi.g6.fetcher.service;
+
+import pi.g6.fetcher.entity.*;
+
+import java.util.List;
+
+public interface CreationService {
+    List<Estado> getEstados();
+    List<PeriodoLetivo> getPeriodos();
+    List<DpucUc> getDPUCs();
+    DpucUc getDPUC(int id);
+    List<UnidadeOrganica> getUOs();
+    List<Curso> getCursos();
+    List<Curso> getCursos(String UO);
+    List<String> getIdiomas();
+    List<Utilizadores> getDocentes();
+    List<AreaCientifica> getAreasCientificas();
+}
