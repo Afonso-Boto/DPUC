497fc9e0e51c87e6292b90c905540a4f0e4b1f30
Jorge Oliveira <jorge.am.oliveira@ua.pt>
2022-05-28 14:34:15 +0100

    Fixed NotFound page

--- DPUC/teacher-app/src/NotFound.js
+++ DPUC/teacher-app/src/NotFound.js
@@ -1,12 +1,21 @@
 import { Link } from "react-router-dom";
-import { Error } from "@paco_ua/pacoui";
+import { InfoBox } from "@paco_ua/pacoui";
+import { React } from "react";
+import { Col, Row } from "react-bootstrap";
 
 const NotFound = () => {
     return ( 
         <div className="not-found" style={{"text-align":"center"}}>
-            <Error text="A página que está a tentar aceder não existe."/>
-            
-            <Link to="/">Voltar à página inicial.</Link>
+            <Row>
+                <Col></Col>
+                <Col md="auto">
+                    <InfoBox title="A página que está a tentar aceder não existe">
+                        <Link to="/">Voltar à página inicial.</Link>
+                    </InfoBox>
+                </Col>
+                <Col></Col>
+            </Row>
+
         </div>
      );
 }
