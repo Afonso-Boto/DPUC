b08e05854d6cc93eb7d0504bc7c1a5b3cf8cbd66
Tomás Candeias <tomaslcandeias@outlook.com>
2022-05-29 16:19:46 +0100

    fetcher->fetcher-main, project modules fixed

---
+++ DPUC/fetcher-main/src/main/java/pi/g6/fetchermain/entity/Estado.java
@@ -0,0 +1,33 @@
+package pi.g6.fetchermain.entity;
+
+public class Estado {
+    private int id;
+    private String nome;
+    private String descricao;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+}
