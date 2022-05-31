a7a83dd5560b802d3ce807d811ce0c1e295a60cc
Renan Alves Ferreira <renanaferreira1999@gmail.com>
2022-05-31 19:02:11 +0100

    Update settings.py

--- DPUC/fetcher_search/fetcher_search/settings.py
+++ DPUC/fetcher_search/fetcher_search/settings.py
@@ -42,13 +42,6 @@ ROOT_URLCONF = CURRENT_DIR + '.urls'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'sqlite3',
-    }
-}
-
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
