09cdcbaf9c31c4c54598f76a87ac4899d8261c51
Renan Alves Ferreira <renanaferreira1999@gmail.com>
2022-05-31 18:20:58 +0100

    Update settings.py

--- DPUC/fetcher_search/fetcher_search/settings.py
+++ DPUC/fetcher_search/fetcher_search/settings.py
@@ -41,8 +41,6 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-
 ]
 
 ROOT_URLCONF = CURRENT_DIR + '.urls'
