2dbaf23538ff63fc1d6613fe98eee2542a1f0d28
Renan Alves Ferreira <renanaferreira1999@gmail.com>
2022-05-31 16:37:59 +0100

    Update urls.py

--- DPUC/fetcher_search/fetcher_search/urls.py
+++ DPUC/fetcher_search/fetcher_search/urls.py
@@ -13,12 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
 
 urlpatterns = [
     path('', lambda r: HttpResponse()),
     path('api/', include('search_api.urls')),
-    path('admin/', admin.site.urls),
 ]
