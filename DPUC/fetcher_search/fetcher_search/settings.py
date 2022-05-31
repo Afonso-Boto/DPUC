2b71cd0cf1b371d0cb7363733920d791296df484
Renan Alves Ferreira <renanaferreira1999@gmail.com>
2022-05-31 16:45:17 +0100

    Revert "Update settings.py"
    This reverts commit 5988ba7f098c7587ac908959ab1ac646821802a4.

--- DPUC/fetcher_search/fetcher_search/settings.py
+++ DPUC/fetcher_search/fetcher_search/settings.py
@@ -74,10 +74,6 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ],
-    'DEFAULT_RENDERER_CLASSES': [
-        'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
     ]
 }
 
@@ -135,3 +131,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ]
+}
