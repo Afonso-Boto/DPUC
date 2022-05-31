5256c032a22ca56d32ed6ff42bc47d061a7e7d11
Renan Alves Ferreira <renanaferreira1999@gmail.com>
2022-05-31 18:47:13 +0100

    Update settings.py

--- DPUC/fetcher_search/fetcher_search/settings.py
+++ DPUC/fetcher_search/fetcher_search/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4ws2!+@gb#noe^1=22_9c)@oyn0!r719t)b_k%^!9b3xfx8-#b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
