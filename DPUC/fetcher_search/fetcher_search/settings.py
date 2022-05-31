af8c7c5c6f35c7cb116782ec52a27e605f0e429d
Renan Alves Ferreira <renanaferreira1999@gmail.com>
2022-05-31 17:52:31 +0100

    Update settings.py

--- DPUC/fetcher_search/fetcher_search/settings.py
+++ DPUC/fetcher_search/fetcher_search/settings.py
@@ -49,22 +49,6 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = CURRENT_DIR + '.urls'
 
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
