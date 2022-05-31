fcd7daf55c779a852b23859ff7ae67368a95d2b6
Renan Alves Ferreira <renanaferreira1999@gmail.com>
2022-05-31 19:24:26 +0100

    relocate key

--- DPUC/fetcher_search/fetcher_search/settings.py
+++ DPUC/fetcher_search/fetcher_search/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4ws2!+@gb#noe^1=22_9c)@oyn0!r719t)b_k%^!9b3xfx8-#b'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
