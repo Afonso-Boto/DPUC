bca0c28493ad756b597309431018c22de803002a
Renan Alves Ferreira <renanaferreira1999@gmail.com>
2022-06-04 18:29:25 +0100

    remove address

--- DPUC/search_api/search_api/src/address.py
+++
@@ -1,9 +0,0 @@
-class Address:
-
-    def __init__(self, host: str, port: int):
-        self.host = host
-        self.port = port
-
-    @property
-    def http_url(self) -> str:
-        return "http://" + self.host + ":" + str(self.port)
