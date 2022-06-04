e42611a1e5c3b4f244676674f2a3990b063a0fd9
Renan Alves Ferreira <renanaferreira1999@gmail.com>
2022-06-04 10:58:36 +0100

    Update manage.py

--- DPUC/fetcher_search/manage.py
+++ DPUC/fetcher_search/manage.py
@@ -7,7 +7,7 @@ APP = 'fetcher_search'
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', APP + '.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', ".".join([APP, "settings"]))
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
