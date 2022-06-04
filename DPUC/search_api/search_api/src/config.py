68aa3d45fb03e1cf6ab6db862d0f26a5c0ba6e06
Renan Alves Ferreira <renanaferreira1999@gmail.com>
2022-06-04 17:56:52 +0100

    Update config.py

--- DPUC/search_api/search_api/src/config.py
+++ DPUC/search_api/search_api/src/config.py
@@ -1,13 +1,3 @@
-import search_api.src.env as env
-
-from search_api.src.mysql import MysqlConnector
-from search_api.src.address import Address
 from search_api.src.es import ElasticSearchConnector
-
-# environment variables
-ES_HOST = env.ES_HOST
-ES_PORT = env.ES_PORT
-INDEX = env.INDEX_NAME
-TTL = env.DAY_MILLISECONDS
 
 es_connector = ElasticSearchConnector()
