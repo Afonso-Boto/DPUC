0782b3a061f07b75edc3ff45f40d8204485bd599
Renan Alves Ferreira <renanaferreira1999@gmail.com>
2022-06-04 10:58:51 +0100

    delete search_api

--- DPUC/fetcher_search/search_api/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SearchApiConfig(AppConfig):
-    name = 'search_api'
--- DPUC/fetcher_search/search_api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
-import search_api.views as views
-
-urlpatterns = [
-    path('search', views.search_dpuc),
-    path('update', views.update)
-]
