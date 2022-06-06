eef0c73ecde27a6283b7a377559f61202617d170
Tomás Candeias <tomaslcandeias@outlook.com>
2022-06-06 23:06:13 +0100

    login/logout

--- DPUC/fetcher-main/src/main/java/pi/g6/fetchermain/controller/LoginController.java
+++ DPUC/fetcher-main/src/main/java/pi/g6/fetchermain/controller/LoginController.java
@@ -1,6 +1,7 @@
 package pi.g6.fetchermain.controller;
 
 import lombok.extern.log4j.Log4j2;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
@@ -14,9 +15,13 @@ import pi.g6.fetchermain.config.JwtRequest;
 import pi.g6.fetchermain.config.JwtResponse;
 import pi.g6.fetchermain.config.JwtTokenUtil;
 import pi.g6.fetchermain.config.JwtUserDetailsService;
+import pi.g6.fetchermain.entity.Utilizadores;
 
 import javax.annotation.PostConstruct;
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/")
@@ -50,16 +55,43 @@ public class LoginController extends JdbcDaoSupport {
 
         final String token = jwtTokenUtil.generateToken(userDetails);
 
+        Utilizadores user = new Utilizadores();
+
         try {
             String sql = "UPDATE utilizadores SET token=?  WHERE email = ?";
             getJdbcTemplate().update(sql, token, authenticationRequest.getUsername());
+
+            String query = "SELECT id, nome, nmec, email, tipo_utilizadorid FROM utilizadores WHERE email = \'%s\'".formatted(authenticationRequest.getUsername());
+            user.setId((int) getJdbcTemplate().queryForList(query).get(0).get("id"));
+            user.setNome((String) getJdbcTemplate().queryForList(query).get(0).get("nome"));
+            user.setNmec((int) getJdbcTemplate().queryForList(query).get(0).get("nmec"));
+            user.setEmail((String) getJdbcTemplate().queryForList(query).get(0).get("email"));
+            user.setTipo_utilizadorid((int) getJdbcTemplate().queryForList(query).get(0).get("tipo_utilizadorid"));
 
         }catch (Exception e){
             log.error("Error while fetching user details from database");
             return ResponseEntity.status(500).body("Error while fetching user details from database");
         }
 
-        return ResponseEntity.ok(new JwtResponse(token));
+        Map<String, String> map = new HashMap<>();
+        map.put("token", token);
+        map.put("user", user.toString());
+
+        return ResponseEntity.ok(map);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(@RequestBody String token) {
+        try {
+            String sql = "UPDATE utilizadores SET token=?  WHERE token = ?";
+            getJdbcTemplate().update(sql, null, token);
+
+        }catch (Exception e){
+            log.error("Error while fetching user details from database");
+            return ResponseEntity.status(500).body("Error while fetching user details from database");
+        }
+
+        return ResponseEntity.ok("Logout successful");
     }
 
     private void authenticate(String username, String password) throws Exception {
