cec2a744d25fa53d5e73e77ced02c41fb6991868
Tomás Candeias <tomaslcandeias@outlook.com>
2022-06-07 13:01:35 +0100

    edicao -> iniciarEdicao + datas_alteracao

--- DPUC/fetcher-main/src/main/java/pi/g6/fetchermain/service/EditionService.java
+++ DPUC/fetcher-main/src/main/java/pi/g6/fetchermain/service/EditionService.java
@@ -11,8 +11,9 @@ import java.util.List;
 public interface EditionService {
     HttpStatus setRegente(int dpucid, int regenteId);
     ResponseEntity<List<DpucUc>> getAllDpucs(int ucid);
-    HttpStatus iniciarEdicao();
+    HttpStatus iniciarEdicao(); // Button para SGA iniciar edição
     ResponseEntity<Integer> definicaoRegente(int dpucid, int regenteId); //E1 DUO
     HttpStatus emEdicao(int dpucid, JSONObject dpuc, boolean finished); //E2 DR
     HttpStatus emAprovacao(int dpucid, boolean aprovado); //E3 DC
+    Dpuc dpucs(int ucid);
 }
