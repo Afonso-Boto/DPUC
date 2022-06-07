b8284fe06f86e6d91cbfc3b7db4aadcd8c7e4137
Theo Magno <theomagno@ua.pt>
2022-06-07 13:07:38 +0100

    Parser do json

--- DPUC/populate-bd/Spark/src/main/scala/test.scala
+++ DPUC/populate-bd/Spark/src/main/scala/test.scala
@@ -19,12 +19,12 @@ object test {
 
     df_1.printSchema()
 
-    df_1.show()
-    //df_1.write.mode(SaveMode.Overwrite).json("/resources/ucs.json")
+    //df_1.show()
+    df_1.write.mode(SaveMode.Overwrite).json("/resources/ucs.json")
 
     val df_2 = df.withColumn("criacao_edicao", lit(0)).withColumn("estado", lit(5)).withColumn("uc_id",monotonically_increasing_id()+17).withColumn("utilizadores_id", lit(0)).select("criacao_edicao", "horas_trabalho", "Objetivos_pt", "Conteudos_pt", "CoerenciaConteudos_pt", "Metodologias_pt", "CoerenciaMetodologias_pt", "Bibliografia_pt", "Observacoes_pt", "regime_faltas", "Requisitos_pt", "Avaliacao_pt", "AprendizagemAtiva_pt", "estado", "semestre", "uc_id", "utilizadores_id")
 
-    df_2.show()
-    //df_2.write.mode(SaveMode.Overwrite).json("/resources/dpucs.json")
+    //df_2.show()
+    df_2.write.mode(SaveMode.Overwrite).json("/resources/dpucs.json")
   }
 }
