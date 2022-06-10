3b54b43332f67ed2bd3a5fc3bc4bbce394d66c24
Afonso Boto <afonsomsboto@gmail.com>
2022-06-10 18:20:49 +0100

    bug fix

--- DPUC/fetcher-main/src/main/java/pi/g6/fetchermain/service/CreationServiceImpl.java
+++ DPUC/fetcher-main/src/main/java/pi/g6/fetchermain/service/CreationServiceImpl.java
@@ -83,7 +83,7 @@ public class CreationServiceImpl extends JdbcDaoSupport implements CreationServi
             query = "SELECT * FROM dpuc JOIN utilizadores on utilizadores.id = dpuc.utilizadoresid WHERE utilizadores.id = " + utilizadorId;
         }
         else if(tipoUtilizador == 1){
-            query = "SELECT * from uc join dpuc on dpuc.UCid = uc.id join unidade_organica where unidade_organica.utilizadoresid = " + utilizadorId;
+            query = "SELECT * from (uc join dpuc on dpuc.UCid = uc.id join unidade_organica on uc.unidade_organicaid = unidade_organica.id) where unidade_organica.utilizadoresid = " + utilizadorId;
         }
 
         List<Map<String, Object>> rows = getJdbcTemplate().queryForList(query);
